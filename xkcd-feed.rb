adb2866e824d511a7491e4cd88e8b852549ea5cc
Joshua McKinney <joshka.git@joshka.net>
2013-01-19 16:03:36 +1100

    change url to /feed, also add to title to easily distinguish right feed in google reader

--- xkcd-feed.rb
+++ xkcd-feed.rb
@@ -1,11 +1,12 @@
 require 'open-uri'
 require 'sinatra'
 
-get '/' do
+get '/feed' do
 	url = 'http://xkcd.com/rss.xml'
 	match = /(&lt;img.*? alt="(?<alt>.*?)".*?&gt;)/
 	replace = "\n\\0\n&lt;p&gt;alt-text: \\k<alt>&lt;/p&gt;\n"
 	open(url) do |page|
-		page.read.gsub(match, replace)
+		feed = page.read.sub(/xkcd.com/, 'xkcd.com - with alt-text')
+		feed.gsub(match, replace)
 	end
 end
