2f5699cac109021dca2ce4b3fdacfbfe110d92ab
Benjamin Oakes <hello@benjaminoakes.com>
2017-02-05 13:26:45 -0600

    Fix RuntimeError at /feed
    XKCD must have switched to requiring HTTPS
    
        RuntimeError at /feed
        redirection forbidden: http://xkcd.com/rss.xml -> https://xkcd.com/rss.xml

--- xkcd-feed.rb
+++ xkcd-feed.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 require 'sinatra'
 
 get '/feed' do
-	url = 'http://xkcd.com/rss.xml'
+	url = 'https://xkcd.com/rss.xml'
 	match = /(&lt;img.*? alt="(?<alt>.*?)".*?&gt;)/
 	replace = "\n\\0\n&lt;p&gt;alt-text: \\k<alt>&lt;/p&gt;\n"
 	open(url) do |page|
